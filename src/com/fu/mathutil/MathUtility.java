9cab413c2888a7c8f0173cb84c206d693c3d8796
zingleeng <thandong.gioinhat.vietnam@gmail.com>
2021-03-25 16:28:35 +0700

    RECURSION UPDATED & GREEN 4EVER AGAIN

--- src/com/fu/mathutil/MathUtility.java
+++ src/com/fu/mathutil/MathUtility.java
@@ -21,14 +21,25 @@ public class MathUtility {
         if (n<0 || n>20)
             throw new IllegalArgumentException("n must be between 0..20");
         
-        //Sống sót đến đây thì n = 0,1..20
+        //sửa code, sửa hàm giai thừa cho gọn, dùng đệ quy
         if (n==0|| n==1)
             return 1; //0! 1! =1
+        return n*getFactorial(n-1);
+        //n!=  n*     (n-1)!
+        //đệ quy, vòng lại chính mình với quy mô khác
+        //recursion. Làm kéo nếu ko nó đi mãi mãi
+        //STACKOVERFLOW.COM
         
-        long result=1; //XANH LOCAL VA SERVER //giả bộ =0
-        for (int i = 2; i <= n; i++) {
-            result *=i;
-        }
-        return result; //nhân từ 2 3 4 5 6 7 ...
+        
+        
+        //Sống sót đến đây thì n = 0,1..20
+//        if (n==0|| n==1)
+//            return 1; //0! 1! =1
+//        
+//        long result=1; //XANH LOCAL VA SERVER //giả bộ =0
+//        for (int i = 2; i <= n; i++) {
+//            result *=i;
+//        }
+//        return result; //nhân từ 2 3 4 5 6 7 ...
     }
 }
